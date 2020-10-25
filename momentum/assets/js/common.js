6d769a07b03d12337b1ef521d541f77ccfc495d6
Aliaksandr Kulik <inter_net@tut.by>
2020-10-25 18:17:21 +0300

    feat: add weather api and quotes

--- momentum/assets/js/common.js
+++ momentum/assets/js/common.js
@@ -8,12 +8,22 @@ let time = document.querySelector(".time"),
 	multiArrRandomNum = [],
 	arrRandomNum = [],
 	partsOfDayArr = ["night", "morning", "day", "evening"],
-	currentHour = (new Date()).getHours();
+	currentHour = (new Date()).getHours(),
+	currentQuoteNum = getRandomIntInclusive(0, quotes.length),
+	quote_content = document.querySelector(".quote_content"),
+	quote_author = document.querySelector(".quote_author"),
 	leftArr = document.querySelector(".sliderL"),
 	rightArr = document.querySelector(".sliderR"),
-	counterSlides = currentHour,
 	counterPartsOfDay = Math.floor(currentHour/6),
-	animation = false;
+	counterSlides = currentHour - counterPartsOfDay*6,
+	animation = false,
+	city = document.querySelector(".weather__city"),
+	wind = document.querySelector(".weather__wind"),
+	temperature = document.querySelector(".weather__temperature"),
+	visibility = document.querySelector(".weather__visibility"),
+	humidity = document.querySelector(".weather__humidity"),
+	description = document.querySelector(".weather__description");
+
 
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; 
@@ -36,7 +46,11 @@ while (multiArrRandomNum.length < 4) {
 	arrRandomNum = [];
  }
 
-
+function changeQuote(num) {
+	quote_content.innerText = quotes[num].text;
+	quote_author.innerText = quotes[num].author;
+}
+changeQuote(currentQuoteNum);
 
 function showTime() {
 	let today = new Date(),
@@ -54,6 +68,8 @@ function showTime() {
 	if (hour !== currentHour) {
 		bgChange();
 		currentHour = hour;
+		currentQuoteNum = getRandomIntInclusive(0, quotes.length);
+		changeQuote(currentQuoteNum);
 	}
 	setTimeout(showTime, 1000)
 }
@@ -96,6 +112,38 @@ function getFocus () {
 	}
 }
 
+function getCity () {
+	if(localStorage.getItem('city') === null) {
+		city.textContent = "[Enter Your City]"
+	} else {
+		city.textContent = localStorage.getItem('city');
+	}
+}
+
+function setCity(e) {
+	if(e.type === `keypress`) {
+		if((e.which == 13 || e.keyCode == 13) && city.textContent !== "") {
+			localStorage.setItem('city', e.target.innerText[0].toUpperCase() + e.target.innerText.slice(1));
+			getWeather()
+			city.blur();
+		} else if ((e.which == 13 || e.keyCode == 13)
+					 && city.textContent === ""
+					 && localStorage.getItem('city') === null) {
+			city.textContent = "[Enter Your City]";
+			getWeather()
+			city.blur();
+		} else if ((e.which == 13 || e.keyCode == 13)
+					 && city.textContent === ""
+					 && localStorage.getItem('city') !== null) {
+			city.textContent = localStorage.getItem('city');
+			getWeather()
+			city.blur();
+		}
+	} else {
+		localStorage.setItem('city', e.target.innerText);
+		getWeather()
+	}
+}
 
 function setName(e) {
 	if(e.type === `keypress`) {
@@ -165,10 +213,23 @@ focus.addEventListener('blur', function(){
 	}	else {focus.textContent = localStorage.getItem('focus')}
 })
 
+city.addEventListener('keypress', setCity)
+city.addEventListener('click', function() {
+	city.textContent = "";
+})
+
+city.addEventListener('blur', function(){
+	if (localStorage.getItem('city') === null) {
+		city.textContent = "[Enter Name]";
+	}	else {city.textContent = localStorage.getItem('city')}
+})
+
 
 
 	leftArr.addEventListener('click', function(){
 		if (animation === false) {
+			currentQuoteNum = getRandomIntInclusive(0, quotes.length);
+			changeQuote(currentQuoteNum);
 			counterSlides--;
 			if (counterSlides < 0) {
 				counterSlides = multiArrRandomNum[0].length - 1;
@@ -178,12 +239,14 @@ focus.addEventListener('blur', function(){
 			animation = true;
 			setTimeout(function(){
 			animation = false;
-			}, 1500)
+			}, 1000)
 		}
 	})
 
 	rightArr.addEventListener('click', function(){
 		if (animation === false) {
+			currentQuoteNum = getRandomIntInclusive(0, quotes.length);
+			changeQuote(currentQuoteNum);
 			counterSlides++;
 			if (counterSlides > multiArrRandomNum[0].length - 1) {
 				counterSlides = 0;
@@ -193,10 +256,37 @@ focus.addEventListener('blur', function(){
 			animation = true;
 			setTimeout(function(){
 			animation = false;
-			}, 1500)
+			}, 1000)
 		}
 	})
 
+
+
+async function getWeather() {
+	const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localStorage.getItem('city')}&lang=en&appid=092dfaf1c1302faae7237e56153d851b&units=metric`);
+	const data = await response.json();
+	console.log(data)
+   try {
+     temperature.textContent = `${data.main.temp.toFixed(1)}°C`
+     wind.textContent = `${data.wind.speed} m/s`
+     humidity.textContent = `${data.main.humidity} %`
+     visibility.textContent = `${data.visibility/1000} km`
+     description.textContent = data.weather[0].description[0].toUpperCase() + data.weather[0].description.slice(1);
+     description.style.backgroundImage = `url('http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png')`
+
+     
+   } catch (e) {
+     if (localStorage.getItem('city') !== null) {
+       city.textContent = "Please enter correct place";
+       wind.textContent ="";
+       humidity.textContent = "";
+       temperature.textContent = "";
+       visibility.textContent = "";
+       description.textContent = "";
+       description.style.backgroundImage = "";
+     } 
+   }
+ }
 
 
 
@@ -204,3 +294,5 @@ showTime()
 bgChange()
 getName()
 getFocus()
+getCity()
+document.addEventListener('DOMContentLoaded', getWeather)
